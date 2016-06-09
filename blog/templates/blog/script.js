d369c2458038030d2fbdffd3c397fbbd310615b5
ankit <ankit.raj.077@gmail.com>
2016-06-10 03:59:50 +0530

    Updated the Html format for file.

---
+++ blog/templates/blog/script.js
@@ -0,0 +1,6 @@
+alert('Welcome! Press OK and enjoy my Work. I hope you will like it. :)');
+
+function myFunction() {
+    alert('Sorry, this is just a View');
+}
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from django.http import HttpResponseRedirect, HttpResponse
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
